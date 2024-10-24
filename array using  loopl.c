c6f793d3b50ed399bea2d3dea8a61a0e12b6481f
Niraj <155253763+Nicode2707@users.noreply.github.com>
2024-10-24 21:10:13 +0530

    Add files via upload

---
+++ array using  loopl.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	
+	int marks[5];
+	for(int i=0;i<5;i++){
+	   printf("enter the marks of student is %d",i+1);
+	}
+	
+		for(int i=0;i<5;i++){
+	   printf(" the value of marks of student is %d is %d",i+1,marks[i]);
+	}
+	
+	return 0;
+}
